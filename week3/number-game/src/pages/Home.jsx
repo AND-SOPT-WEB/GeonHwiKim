b8f4e101921cb8f7e1c3aa10894544b4dbf941d9
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-11-01 02:54:19 +0900

    style: HomePageWrapper 스타일링

--- week3/number-game/src/pages/Home.jsx
+++ week3/number-game/src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import HomeHeader from "../components/Home/HomeHeader";
+import styled from "styled-components";
 
 export const Home = () => {
-  return <HomeHeader />;
+  return (
+    <HomePageWrapper>
+      <HomeHeader />
+    </HomePageWrapper>
+  );
 };
+
+const HomePageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  background: ${({ theme }) => theme.colors.orange2};
+`;
