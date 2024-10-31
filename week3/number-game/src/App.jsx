9b62390a51481cbf924c509d3e2312f7b8a6a272
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-11-01 02:15:26 +0900

    chore: 글로벌 스타일, theme 설정

--- week3/number-game/src/App.jsx
+++ week3/number-game/src/App.jsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <div>App</div>;
-};
+import GlobalStyle from "../styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
+import Router from "./Router";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  );
+}
 
 export default App;
---
+++ week3/number-game/styles/theme.js
@@ -0,0 +1,33 @@
+const colors = {
+  orange1: "#F15A25",
+  orange2: "rgba(209, 68, 7, 0.24)",
+  orange3: "#FFE5D9",
+  orange4: "#AA4623",
+
+  white1: "#FFFFFF",
+  white2: "#DEDEDE",
+  white3: "#FCFCFC",
+
+  gray1: "#3B3B3B",
+  gray2: "#5C5C5C",
+  gray3: "#A6ABBC",
+
+  background: "#000000",
+  background_box: "#212121",
+
+  article_box: "rgba(217, 217, 217, 0.10)",
+  article_content: "rgba(119, 119, 119, 0.63)",
+
+  bookmark_hover: "#343434",
+  bookmark_click: "#6E4A39",
+  bookmark_select: "#FFE5D9",
+
+  thumnail: "#D9D9D9",
+  transparent: "rgba(0,0,0,0)",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
