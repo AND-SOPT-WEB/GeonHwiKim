56ff2d40e3e7c7e9b234f8119761490b19a7ba2b
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-11-11 03:53:18 +0900

    style: outlet 스타일링

--- week4/week4_assignment/src/Roots.tsx
+++ week4/week4_assignment/src/Roots.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 const Roots = () => {
   return (
-    <>
+    <OutletContainer>
       <Outlet />
-    </>
+    </OutletContainer>
   );
 };
 
 export default Roots;
+
+const OutletContainer = styled.main`
+  width: 100%;
+  height: 100dvh;
+  display: flex;
+  justify-content: center;
+
+  background: ${({ theme }) => theme.colors.orange7};
+`;
